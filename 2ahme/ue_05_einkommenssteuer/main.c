cbc0807c28368b3a5e0c26f0bccedd2bcb47d833
hoeelm18 <hoeelm18@htl-kaindorf.at>
2020-02-27 21:43:49 +0100

    schleifen

--- 2ahme/ue_05_einkommenssteuer/main.c
+++ 2ahme/ue_05_einkommenssteuer/main.c
@@ -10,8 +10,12 @@ int main()
     printf("UE05 Einkommensteuer\n");
     printf("-----------------------\n");
 
+
+    printf("Verfuegbares Einkommen:\n");
     fgets(s, 80, stdin);
     sscanf(s, "%lf", &einkommen);
+
+
     if (einkommen < 0) {
         printf("Eingabefehler\n");
         return 1;
@@ -23,18 +27,27 @@ int main()
         steuer = steuer + 30000 * 0.48;
         steuer = steuer + 29000 * 0.42;
 
+        printf("Steuer = %.2lf", steuer);
+
     } else if (einkommen >= 90000) {
         steuer = steuer + 30000 * 0.48;
         steuer = steuer + 29000 * 0.42;
 
+        printf("Steuer = %.2lf", steuer);
+
     } else if (einkommen >= 60000) {
         steuer = steuer + 29000 * 0.42;
+
+        printf("Steuer = %.2lf", steuer);
 
     } else if (einkommen >= 31000) {
         steuer = steuer + 31000 * 0.42;
 
+        printf("Steuer = %.2lf", steuer);
+
     } else {
         steuer = 0;
+        printf("Steuer = %.2lf", steuer);
     }
 
     return 0;
