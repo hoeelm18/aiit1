b07b1e86ad280299afd3b40d0171c9fa8a2c1ce0
hoeelm18 <hoeelm18@htl-kaindorf.at>
2021-02-22 00:01:03 +0100

    Labor

--- Labor/ue02/main.c
+++ Labor/ue02/main.c
@@ -95,18 +95,96 @@ void eingabe(int *anzPart, int *anzMand, int stimmen[], char namen[][MAXNAMLEN])
 
 void baueTabelle(int anzPart, int anzMand, int stimmen[], double tabelle[][MAXPART]){
   
+  double max = 0;
+  int merker = 0;
+  int divisor;
+  int zaehler = 0;
   
+   for (int j = 0; j < anzPart; j++) {
+      divisor = 1;
+      
+   for (int i = 0; i < anzMand; i++ ) {
+      tabelle[i][j] = (double)stimmen[zaehler]/(double)(divisor);
+      divisor++;
+      
+     }
+      
+    zaehler++;
+    
+   }
   
+     max = tabelle[0][0];
+
+}
+  
+void gibTabelleAus(int anzPart, int anzMand, double tabelle[][MAXPART]) {
+  
+    int j = 0;
+    
+      for (; j < anzMand; j++) {
+      printf("\n");
+      
+      for (int i = 0; i < anzPart; i++) {
+      printf("%12.3lf |", tabelle[j][i]);
+      
+    }
+  }
+}
+
+void findeHoechstzahl(double tabelle[][MAXPART], int anzPart, int anzMand, int*zeile, int*spalte){
+  
+  int* zeile, int* spalte){
+    double max = tabelle[0][0];
+    *zeile = 0;
+    *spalte = 0;
+
+    for(int i = 0; i < anzMand; i++){
+        for(int j = 0; j < anzPart; j++){
+         if(tabelle[i][j] > max) {
+            max = tabelle[i][j];
+            *zeile = i;
+            *spalte = j;
+            }
+        }
+    }
+}
+
   
 }
 
 
+void dhondt(double tabelle[][MAXPART], int anzPart, int anzMand, int mandate[]){
+  
+    int zeile, spalte;
 
-void gibTabelleAus(int anzPart, int anzMand, double tabelle[][MAXPART]){
+    for(int i=0; i<anzMand; i++) {
+    mandate[i]= 0;
+    }
+    for(int j = 0; j<anzMand;j++){
+        findeHoechsteZahl (tabelle, anzPart, anzMand, &zeile, &spalte);
+        mandate[spalte] ++;
+        tabelle[zeile][spalte] = 0;
+    }
+}
+
+void ausgabe(int anzPart, int anzMand, int mandate[], int stimmen[], char name[][MAXNAMLEN]) {
   
-  
-  
-  
+double gesamtstimmen = 0, hiv;
+
+    printf("\n\n  Partei  | Stimmen | Prozent | Mandate"\n---------------------------------------");
+
+    for(int i=0;i<anzPart;i++){
+      
+    gesamtstimmen += stimmen[i];
+    
+    }
+
+    for(int i=0; i<anzPart;i++){
+      
+    hiv = 100.0 * stimmen[i]/gesamtstimmen;
+    printf("\n %8s | %7d | %7.2lf | %7d", name [i], stimmen[i], hiv, mandate[i]);
+    
+    }
 }
 
 
@@ -123,5 +201,11 @@ int main(){
    
    eingabe(&anzPart, &anzMand, stimmen, name);
 
+   baueTabelle(anzPart, anzMand, stimmen, tabelle);
+   gibTabelleAus(anzPart, anzMand, tabelle);
+   dhondt(tabelle, anzPart, anzMand, mandate);
+   ausgabe(anzPart, anzMand, mandate, stimmen, name);
+   
+   
     return 0;
 }
