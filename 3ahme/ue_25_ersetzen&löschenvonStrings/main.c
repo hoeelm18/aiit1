955b9bf446e0b9d81586dfa4a4369b0990e0aa0c
hoeelm18.Elias <hoeelm18@htl-kaindorf.at>
2021-03-18 14:55:08 +0100

    ersetzenlöschenstrings

---
+++ 3ahme/ue_25_ersetzen&löschenvonStrings/main.c
@@ -0,0 +1,62 @@
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int replace(char* str_source, char* str_search, char* str_replace){
+    
+    char string[1000] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!";
+    char search[] = "ABC";
+    char replace[] = "Strings";
+
+}
+int delete(char* str_source, char* strc_delete){
+
+    
+	char string[100] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!";
+    for(int i = 0, i < 1){
+	char search[] = "nicht";
+	char replace[] = "";
+	}
+	
+
+	return replace[];
+}
+    
+    
+  
+int main (int argc, char** argv) {
+    
+    
+  
+    
+    return (EXIT_SUCCESS);
+}
+
+/*int replace(char* str_source, char* str_search, char* str_replace){
+    
+    char *txt [] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!";
+    
+}
+
+
+int delete(char* str_source, char* strc_delete){
+
+    char string[] = *txt;
+char needle[] = "nicht";
+
+if(strstr(string, needle)) {
+	printf("String enthaelt '123'\n");
+}
+    
+}
+int main (int argc, char** argv) {
+    
+    
+  //  "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!
+    
+    
+    return (EXIT_SUCCESS);
+}
+*/ 
