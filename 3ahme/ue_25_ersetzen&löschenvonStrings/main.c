b36496b1bfa4f2d0ede068999625498d4e81317b
hoeelm18 <hoeelm18@htl-kaindorf.at>
2021-03-24 20:55:19 +0100

    Hüue25

--- 3ahme/ue_25_ersetzen&löschenvonStrings/main.c
+++ 3ahme/ue_25_ersetzen&löschenvonStrings/main.c
@@ -1,62 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void strreplace(char* str_source, char* str_search, char* str_replace){
+
+char* adresse = 0;
+int element = 0;
+char f[200];
+int l1 = 0;
+int l2 = 0;
+int l3 = 0;
 
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+  do{
 
-int replace(char* str_source, char* str_search, char* str_replace){
-    
-    char string[1000] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!";
-    char search[] = "ABC";
-    char replace[] = "Strings";
+    adresse = strstr(str_source, str_search);
+    element = adresse - str_source;
 
-}
-int delete(char* str_source, char* strc_delete){
+    strncpy(f, str_source, element);
+    strcpy(f + element, str_replace);
 
-    
-	char string[100] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!";
-    for(int i = 0, i < 1){
-	char search[] = "nicht";
-	char replace[] = "";
-	}
-	
+    l1 = strlen(str_replace);
+    l2 = strlen(str_search);
+    l3 = strlen(str_source);
 
-	return replace[];
-}
-    
-    
-  
-int main (int argc, char** argv) {
-    
-    
-  
-    
-    return (EXIT_SUCCESS);
-}
+    strncpy(f + (element + l1), str_source + (element + l2), (l3 - element));
+    strcpy(str_source, f);
 
-/*int replace(char* str_source, char* str_search, char* str_replace){
-    
-    char *txt [] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!";
-    
+    }while(strstr(str_source, str_search) != NULL);
+
+      l3 = strlen(str_source);
+      str_source[l3 + 1] = '\0';
 }
 
+void strdelete(char* str_source, char* strc_delete){
 
-int delete(char* str_source, char* strc_delete){
+char* adresse = 0;
+int element = 0;
+char f[200];
+int l1 = 0;
+int l2 = 0;
 
-    char string[] = *txt;
-char needle[] = "nicht";
+    l1 = strlen(str_source);
+    l2 = strlen(strc_delete);
 
-if(strstr(string, needle)) {
-	printf("String enthaelt '123'\n");
+   do{
+
+    adresse = strstr(str_source, strc_delete);
+    element = adresse - str_source;
+
+    strcpy(f, str_source);
+
+    for(int i = 0; i < element + 1; i++){
+      if(i == element){
+        for(int j = element - 1; j < l1 + 1; j++)
+        strcpy(str_source + j, f + (j + l2 + 1));
+        }
+      }
+    }while(strstr(str_source, strc_delete) != NULL);
+  }
+
+int main(){
+
+char beliebigername[200] = "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbstständig gelöst habe!";
+char str_search[] = "ABC";
+char str_replace[] = "Strings";
+
+printf("\nAusgangssatz:\n%s\n", beliebigername);
+
+strreplace(beliebigername, str_search, str_replace);
+printf("\nAufgabe 1:\n%s\n", beliebigername);
+
+strdelete(beliebigername, "nicht");
+printf("\nAufgabe 2:\n%s\n", beliebigername);
+
+
+    return 0;
 }
-    
-}
-int main (int argc, char** argv) {
-    
-    
-  //  "Ich kenne mich nicht gut mit ABC aus, da ich das Programm zum Thema ABC nicht selbsständig gelöst habe!
-    
-    
-    return (EXIT_SUCCESS);
-}
-*/ 
